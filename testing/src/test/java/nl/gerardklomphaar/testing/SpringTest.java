52bd88e51069f09f892a323711d4a0102529859d
SupaGait <gklomphaar@gmail.com>
2016-03-22 19:04:05 +0100

    Previous class
    Included testing.

--- testing/src/test/java/nl/gerardklomphaar/testing/SpringTest.java
+++ testing/src/test/java/nl/gerardklomphaar/testing/SpringTest.java
@@ -7,18 +7,30 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import fr.shazilgerard.findmypatient.dao.IDataDAO;
+import fr.shazilgerard.findmypatient.dao.PatientJDBCDAO;
+
 @RunWith(SpringJUnit4ClassRunner.class)
 //@ContextConfiguration(classes = AppConfig.class, loader = AnnotationConfigContextLoader.class)
 @ContextConfiguration(locations={"application-context.xml"})
 public class SpringTest {
 	
+	//@Autowired
+	//TestClass testclass;
+	
 	@Autowired
-	TestClass testclass;
+	IDataDAO<PatientJDBCDAO> patientDao;
 	
 	@Test
-	public void test1()
+	public void testHappyFail()
 	{
 		Assert.fail("I failed happy");
+	}
+	
+	@Test
+	public void testPatientDao()
+	{
+		// use the patientDAO
 	}
 }
 
