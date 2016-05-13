07048ae8b369045fd13119c636925585bc7717a8
SupaGait <gklomphaar@gmail.com>
2016-05-13 15:24:45 +0200

    Removed older folders

--- testpatterns/src/main/java/nl/gerardklomphaar/testpatterns/messageFactory/MessageInterface.java
+++
@@ -1,8 +0,0 @@
-package nl.gerardklomphaar.testpatterns.messageFactory;
-
-public interface MessageInterface {
-
-	public String getMessageName();
-	public String getContext();
-	
-}
--- testpatterns/src/main/java/nl/gerardklomphaar/testpatterns/messageFactory/MessageTypes.java
+++
@@ -1,6 +0,0 @@
-package nl.gerardklomphaar.testpatterns.messageFactory;
-
-public enum MessageTypes {
-		ErrorMessage,
-		InfoMessage
-}
